eb62893ad668352faded0c096af9c9dfc7ff7c7e
Euan <bell_designs@outlook.com>
2018-06-13 16:06:22 +0100

    created test getCapacity

---
+++ java.code.test/src/main/java/Basket.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Basket {
+
+    private ArrayList<Item>shoppingBasket;
+
+    public Basket(){
+        this.shoppingBasket = new ArrayList();
+    }
+
+    public int getCapacity() {
+       return  shoppingBasket.size();
+    }
+}
--- java.code.test/src/test/java/basketTest.java
+++ java.code.test/src/test/java/basketTest.java
@@ -1,6 +1,25 @@
-//import java.util.ArrayList;
-//
-//public class basketTest {
-//
-//    ArrayList<>
-//}
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class basketTest {
+
+Basket shoppingBasket;
+
+@Before
+public void before(){
+    shoppingBasket = new Basket();
+}
+
+
+@Test
+    public void canCheckShoppingBasketCapacity(){
+    assertEquals(0,shoppingBasket.getCapacity());
+
+}
+}
