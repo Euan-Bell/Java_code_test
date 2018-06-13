b25e4dd943966fa957ce707b6192f32363648704
Euan <bell_designs@outlook.com>
2018-06-13 15:31:32 +0100

    set up class files item, basket, itemTest, basketTest

---
+++ java.code.test/src/test/java/itemTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class itemTest {
+
+ Item item;
+ Item item2;
+
+ @Before
+    public void before(){
+     item = new Item("Beans", 05.00);
+     item2 = new Item("Toast", 02.50);
+ }
+
+ @Test
+    public void canGetItemName(){
+     assertEquals("Beans", item.getName());
+ }
+}
