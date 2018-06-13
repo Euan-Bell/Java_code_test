19a7449ff7853b869821e81b72da914e95277381
Euan <bell_designs@outlook.com>
2018-06-13 16:35:05 +0100

    added add and remove severalItems

--- java.code.test/src/test/java/basketTest.java
+++ java.code.test/src/test/java/basketTest.java
@@ -10,11 +10,13 @@ public class basketTest {
 
 Basket shoppingBasket;
 Item item;
+Item item2;
 
 @Before
 public void before(){
     shoppingBasket = new Basket();
     item = new Item("Spam", 01.00);
+    item2 = new Item("Cheese", 02.00);
 }
 
 
@@ -29,10 +31,26 @@ public void before(){
     assertEquals(1,shoppingBasket.getCapacity());
     }
 
+@Test
+    public void canAddSeveralItemsToBasket(){
+    shoppingBasket.addItem(item);
+    shoppingBasket.addItem(item2);
+    assertEquals(2,shoppingBasket.getCapacity());
+    }
+
  @Test
     public void canRemoveItemFromBasket(){
     shoppingBasket.addItem(item);
     shoppingBasket.removeItem(item);
+    assertEquals(0,shoppingBasket.getCapacity());
+    }
+
+@Test
+    public void canRemoveSeveralItemsFromBasket(){
+    shoppingBasket.addItem(item);
+    shoppingBasket.addItem(item2);
+    shoppingBasket.removeItem(item);
+    shoppingBasket.removeItem(item2);
     assertEquals(0,shoppingBasket.getCapacity());
     }
 }
